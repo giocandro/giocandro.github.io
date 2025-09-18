52d64c18fa569a8e61e898e656870734ccabe205
giocandro <alessandroscafi@hotmail.it>
2025-09-18 16:08:39 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menu = document.getElementById("menu");
   menu.innerHTML = `
-    <button class="menu-toggle">☰</button>
+    <button class="menu-toggle">Menù ☰</button>
     <nav>
       <a href="https://giocandro.github.io/index.html">Home</a>
       <a href="https://giocandro.github.io/games/snake/index.html">Snake</a>
